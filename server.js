9430baee9dcc3231b695fad80f6a4ee769b1637c
Karthikraj-S <karthiksuvines11@gmail.com>
2026-04-15 12:26:04 +0530

    update login route

--- server.js
+++ server.js
@@ -14,13 +14,7 @@ app.use(express.json());
    DATABASE CONNECTION
 ========================= */
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((err) => {
   if (err) {
